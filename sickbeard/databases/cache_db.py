9f5b22e9f8408c6242b1bbdd02c45148a33774ab
Giovanni <gborri@libero.it>
2014-07-16 17:39:43 +0200

    Fix problem on migrate cache.db database

--- sickbeard/databases/cache_db.py
+++ sickbeard/databases/cache_db.py
@@ -17,6 +17,18 @@
 # along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
 
 from sickbeard import db
+from sickbeard import logger, helpers
+
+MIN_DB_VERSION = 1 # oldest db version we support migrating from
+MAX_DB_VERSION = 6
+
+def backupDatabase(version):
+    logger.log(u"Backing up database before upgrade")
+    if not helpers.backupVersionedFile(db.dbFilename(), version):
+        logger.log_error_and_exit(u"Database backup failed, abort upgrading database")
+    else:
+        logger.log(u"Proceeding with upgrade")
+
 
 # Add new migrations at the bottom of the list; subclass the previous migration.
 class InitialSchema(db.SchemaUpgrade):
@@ -25,18 +37,32 @@ class InitialSchema(db.SchemaUpgrade):
 
     def execute(self):
 
-        queries = [
-            ("CREATE TABLE lastUpdate (provider TEXT, time NUMERIC);",),
-            ("CREATE TABLE lastSearch (provider TEXT, time NUMERIC);",),
-            ("CREATE TABLE db_version (db_version INTEGER);",),
-            ("INSERT INTO db_version (db_version) VALUES (?)", 1),
-        ]
-        for query in queries:
-            if len(query) == 1:
-                self.connection.action(query[0])
-            else:
-                self.connection.action(query[0], query[1:])
+        if not self.hasTable("lastUpdate") and not self.hasTable("db_version"):
+            queries = [
+                ("CREATE TABLE lastUpdate (provider TEXT, time NUMERIC);",),
+                ("CREATE TABLE lastSearch (provider TEXT, time NUMERIC);",),
+                ("CREATE TABLE db_version (db_version INTEGER);",),
+                ("INSERT INTO db_version (db_version) VALUES (?)", 1),
+            ]
+            for query in queries:
+                if len(query) == 1:
+                    self.connection.action(query[0])
+                else:
+                    self.connection.action(query[0], query[1:])
+        else:
+            cur_db_version = self.checkDBVersion()
 
+            if cur_db_version < MIN_DB_VERSION:
+                logger.log_error_and_exit(u"Your cache database version (" + str(cur_db_version) + ") is too old to migrate from what this version of Sick Beard supports (" + \
+                                          str(MIN_DB_VERSION) + ").\n" + \
+                                          "Upgrade using a previous version (tag) build 496 to build 501 of Sick Beard first or remove database file to begin fresh."
+                                          )
+
+            if cur_db_version > MAX_DB_VERSION:
+                logger.log_error_and_exit(u"Your cache database version (" + str(cur_db_version) + ") has been incremented past what this version of Sick Beard supports (" + \
+                                          str(MAX_DB_VERSION) + ").\n" + \
+                                          "If you have used other forks of Sick Beard, your database may be unusable due to their modifications."
+                                          )
 
 class AddSceneExceptions(InitialSchema):
     def test(self):
@@ -44,14 +70,14 @@ class AddSceneExceptions(InitialSchema):
 
     def execute(self):
         self.connection.action(
-            "CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, indexer_id INTEGER KEY, show_name TEXT)")
+            "CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, tvdb_id INTEGER KEY, show_name TEXT)")
 
 class AddSceneNameCache(AddSceneExceptions):
     def test(self):
         return self.hasTable("scene_names")
 
     def execute(self):
-        self.connection.action("CREATE TABLE scene_names (indexer_id INTEGER, name TEXT)")
+        self.connection.action("CREATE TABLE scene_names (tvdb_id INTEGER, name TEXT)")
 
 
 class AddNetworkTimezones(AddSceneNameCache):
@@ -68,24 +94,70 @@ class AddLastSearch(AddNetworkTimezones):
     def execute(self):
         self.connection.action("CREATE TABLE lastSearch (provider TEXT, time NUMERIC)")
 
-class AddSceneExceptionsSeasons(AddSceneNameCache):
+class AddSceneExceptionsSeasons(AddLastSearch):
     def test(self):
-        return self.hasColumn("scene_exceptions", "season")
+        return self.checkDBVersion() >= 2
 
     def execute(self):
-        self.addColumn("scene_exceptions", "season", "NUMERIC", -1)
+        backupDatabase(self.checkDBVersion())
+
+        if not self.hasColumn("scene_exceptions", "season"):
+            self.addColumn("scene_exceptions", "season", "NUMERIC", -1)
+
+        self.incDBVersion()
 
 class AddSceneExceptionsCustom(AddSceneExceptionsSeasons):
     def test(self):
-        return self.hasColumn("scene_exceptions", "custom")
+        return self.checkDBVersion() >= 3
 
     def execute(self):
-        self.addColumn("scene_exceptions", "custom", "NUMERIC", 0)
+        backupDatabase(self.checkDBVersion())
+
+        if not self.hasColumn("scene_exceptions", "custom"):
+            self.addColumn("scene_exceptions", "custom", "NUMERIC", 0)
+
+        self.incDBVersion()
 
 class AddSceneExceptionsRefresh(AddSceneExceptionsCustom):
     def test(self):
-        return self.hasTable("scene_exceptions_refresh")
+        return self.checkDBVersion() >= 4
 
     def execute(self):
-        self.connection.action(
-            "CREATE TABLE scene_exceptions_refresh (list TEXT PRIMARY KEY, last_refreshed INTEGER)")
+        backupDatabase(self.checkDBVersion())
+
+        if self.hasTable("scene_exceptions_refresh"):
+            self.connection.action(
+                "CREATE TABLE scene_exceptions_refresh (list TEXT PRIMARY KEY, last_refreshed INTEGER)")
+
+        self.incDBVersion()
+
+class MigrateTvdbidToIndexerIdInScheneNames(AddSceneExceptionsRefresh):
+    def test(self):
+        return self.checkDBVersion() >= 5
+ 
+    def execute(self):
+        backupDatabase(self.checkDBVersion())
+
+        if not self.hasColumn("scene_names", "indexer_id"):
+            self.connection.action("ALTER TABLE scene_names RENAME TO tmp_scene_names")
+            self.connection.action("CREATE TABLE scene_names (indexer_id INTEGER, name TEXT)")
+            self.connection.action("INSERT INTO scene_names(indexer_id, name) SELECT tvdb_id, name FROM tmp_scene_names")
+            self.connection.action("DROP TABLE tmp_scene_names")
+
+        self.incDBVersion()
+
+class MigrateTvdbidToIndexerIdInSceneExceptions(MigrateTvdbidToIndexerIdInScheneNames):
+    def test(self):
+        return self.checkDBVersion() >= 6
+
+    def execute(self):
+        backupDatabase(self.checkDBVersion())
+
+        if not self.hasColumn("scene_exceptions", "indexer_id"):
+            self.connection.action("ALTER TABLE scene_exceptions RENAME TO tmp_scene_exceptions")
+            self.connection.action("CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, indexer_id INTEGER KEY, show_name TEXT)")
+            self.connection.action("INSERT INTO scene_exceptions(exception_id, indexer_id, show_name) SELECT exception_id, tvdb_id, show_name FROM tmp_scene_exceptions")
+            self.connection.action("DROP TABLE tmp_scene_exceptions")
+
+        self.incDBVersion()
+
