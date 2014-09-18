3ddddf68888d31565e30c0c0a9a61f2937b6d95f
Giovanni <gborri@libero.it>
2014-09-18 18:33:44 +0200

    Fix saveSearch() got an unexpected keyword argument 'downloadable_search_days'

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1637,7 +1637,7 @@ class ConfigSearch(MainHandler):
     def saveSearch(self, use_nzbs=None, use_torrents=None, nzb_dir=None, sab_username=None, sab_password=None,
                    sab_apikey=None, sab_category=None, sab_host=None, nzbget_username=None, nzbget_password=None,
                    nzbget_category=None, nzbget_priority=None, nzbget_host=None, nzbget_use_https=None,
-                   backlog_days=None, backlog_frequency=None, downloadable_search_frequency=None, dailysearch_frequency=None, 
+                   backlog_days=None, backlog_frequency=None, downloadable_search_days=None, downloadable_search_frequency=None, dailysearch_frequency=None, 
                    nzb_method=None, torrent_method=None, usenet_retention=None,
                    download_propers=None, check_propers_interval=None, allow_high_priority=None,
                    backlog_startup=None, downloadable_search_startup=None, dailysearch_startup=None,
